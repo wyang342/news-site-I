5857302293c6c8fc81ce4ccdad3accba296a58ab
Jonathan Young <jonyoungg@gmail.com>
2017-04-13 15:48:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
         <ArticleTeaser 
           id={this.state.article.id}
           title={this.state.article.title}
-          created_date={this.state.article.created_date}
+          createdDate={this.state.article.createdDate}
           handleTitleClick={(articleID) => console.log(articleID)} />
         <h1>Article Component</h1>
         <hr />
